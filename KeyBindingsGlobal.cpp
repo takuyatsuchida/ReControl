785a0cdf619ed8890c91deaaefb598125b643c2a
Takuya Tsuchida <69303076+takuyatsuchida@users.noreply.github.com>
2025-03-09 10:51:56 +0900

    feat: update global key bindings

--- KeyBindingsGlobal.cpp
+++ KeyBindingsGlobal.cpp
@@ -1,6 +1,11 @@
 #include "pch.h"
 #include "KeyBindingsGlobal.h"
 
+namespace
+{
+    constexpr DWORD KEYEVENTF_KEYDOWN = 0x0000;
+} // namespace
+
 namespace ReControl::KeyBindings
 {
     // Global key bindings are inspired by PSReadLine's Emacs key bindings
@@ -9,17 +14,205 @@ namespace ReControl::KeyBindings
     {
         if (wParam == WM_KEYDOWN)
         {
-            switch (const auto key = reinterpret_cast<KBDLLHOOKSTRUCT *>(lParam); key->vkCode)
+            switch (const auto key = reinterpret_cast<KBDLLHOOKSTRUCT *>(lParam); key->vkCode) // NOLINT(performance-no-int-to-ptr)
             {
-            case 'A': // RCtrl + A -> Home : BeginningOfLine
+            case 'A': { // RControl + A -> Home : BeginningOfLine
                 if (!(GetAsyncKeyState(VK_RCONTROL) & 0x8000)) return false;
-                MessageBox(nullptr, L"Ctrl + A", L"Key Binding", MB_OK | MB_ICONINFORMATION);
+
+                auto inputs = std::array{
+                    // clang-format off
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_RCONTROL, .dwFlags = KEYEVENTF_KEYUP   | KEYEVENTF_EXTENDEDKEY}},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_HOME,     .dwFlags = KEYEVENTF_KEYDOWN                        }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_HOME,     .dwFlags = KEYEVENTF_KEYUP                          }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_RCONTROL, .dwFlags = KEYEVENTF_KEYDOWN | KEYEVENTF_EXTENDEDKEY}},
+                    // clang-format on
+                };
+                SendInput(static_cast<UINT>(inputs.size()), inputs.data(), sizeof(INPUT));
+
                 return true;
+            }
+
+            case 'B': { // RControl + B -> Left : BackwardChar
+                if (!(GetAsyncKeyState(VK_RCONTROL) & 0x8000)) return false;
+
+                auto inputs = std::array{
+                    // clang-format off
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_RCONTROL, .dwFlags = KEYEVENTF_KEYUP   | KEYEVENTF_EXTENDEDKEY}},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_LEFT,     .dwFlags = KEYEVENTF_KEYDOWN                        }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_LEFT,     .dwFlags = KEYEVENTF_KEYUP                          }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_RCONTROL, .dwFlags = KEYEVENTF_KEYDOWN | KEYEVENTF_EXTENDEDKEY}},
+                    // clang-format on
+                };
+                SendInput(static_cast<UINT>(inputs.size()), inputs.data(), sizeof(INPUT));
+
+                return true;
+            }
+
+            case 'D': { // RControl + D -> Delete : DeleteChar
+                if (!(GetAsyncKeyState(VK_RCONTROL) & 0x8000)) return false;
+
+                auto inputs = std::array{
+                    // clang-format off
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_RCONTROL, .dwFlags = KEYEVENTF_KEYUP   | KEYEVENTF_EXTENDEDKEY}},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_DELETE,   .dwFlags = KEYEVENTF_KEYDOWN                        }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_DELETE,   .dwFlags = KEYEVENTF_KEYUP                          }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_RCONTROL, .dwFlags = KEYEVENTF_KEYDOWN | KEYEVENTF_EXTENDEDKEY}},
+                    // clang-format on
+                };
+                SendInput(static_cast<UINT>(inputs.size()), inputs.data(), sizeof(INPUT));
+
+                return true;
+            }
+
+            case 'E': { // RControl + E -> End : EndOfLine
+                if (!(GetAsyncKeyState(VK_RCONTROL) & 0x8000)) return false;
+
+                auto inputs = std::array{
+                    // clang-format off
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_RCONTROL, .dwFlags = KEYEVENTF_KEYUP   | KEYEVENTF_EXTENDEDKEY}},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_END,      .dwFlags = KEYEVENTF_KEYDOWN                        }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_END,      .dwFlags = KEYEVENTF_KEYUP                          }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_RCONTROL, .dwFlags = KEYEVENTF_KEYDOWN | KEYEVENTF_EXTENDEDKEY}},
+                    // clang-format on
+                };
+                SendInput(static_cast<UINT>(inputs.size()), inputs.data(), sizeof(INPUT));
+
+                return true;
+            }
+
+            case 'F': { // RControl + F -> Right : ForwardChar
+                if (!(GetAsyncKeyState(VK_RCONTROL) & 0x8000)) return false;
+
+                auto inputs = std::array{
+                    // clang-format off
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_RCONTROL, .dwFlags = KEYEVENTF_KEYUP   | KEYEVENTF_EXTENDEDKEY}},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_RIGHT,    .dwFlags = KEYEVENTF_KEYDOWN                        }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_RIGHT,    .dwFlags = KEYEVENTF_KEYUP                          }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_RCONTROL, .dwFlags = KEYEVENTF_KEYDOWN | KEYEVENTF_EXTENDEDKEY}},
+                    // clang-format on
+                };
+                SendInput(static_cast<UINT>(inputs.size()), inputs.data(), sizeof(INPUT));
+
+                return true;
+            }
+
+            case 'H': { // RControl + H -> Backspace : BackwardDeleteChar
+                if (!(GetAsyncKeyState(VK_RCONTROL) & 0x8000)) return false;
+
+                auto inputs = std::array{
+                    // clang-format off
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_RCONTROL, .dwFlags = KEYEVENTF_KEYUP   | KEYEVENTF_EXTENDEDKEY}},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_BACK,     .dwFlags = KEYEVENTF_KEYDOWN                        }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_BACK,     .dwFlags = KEYEVENTF_KEYUP                          }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_RCONTROL, .dwFlags = KEYEVENTF_KEYDOWN | KEYEVENTF_EXTENDEDKEY}},
+                    // clang-format on
+                };
+                SendInput(static_cast<UINT>(inputs.size()), inputs.data(), sizeof(INPUT));
+
+                return true;
+            }
+
+            case 'K': { // RControl + K -> Shift + End, Shift + Delete : KillLine
+                if (!(GetAsyncKeyState(VK_RCONTROL) & 0x8000)) return false;
+
+                auto inputs = std::array{
+                    // clang-format off
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_RCONTROL, .dwFlags = KEYEVENTF_KEYUP   | KEYEVENTF_EXTENDEDKEY}},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_SHIFT,    .dwFlags = KEYEVENTF_KEYDOWN                        }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_END,      .dwFlags = KEYEVENTF_KEYDOWN                        }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_END,      .dwFlags = KEYEVENTF_KEYUP                          }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_DELETE,   .dwFlags = KEYEVENTF_KEYDOWN                        }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_DELETE,   .dwFlags = KEYEVENTF_KEYUP                          }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_SHIFT,    .dwFlags = KEYEVENTF_KEYUP                          }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_RCONTROL, .dwFlags = KEYEVENTF_KEYDOWN | KEYEVENTF_EXTENDEDKEY}},
+                    // clang-format on
+                };
+                SendInput(static_cast<UINT>(inputs.size()), inputs.data(), sizeof(INPUT));
+
+                return true;
+            }
+
+            case 'N': { // RControl + N -> Down : NextLine
+                if (!(GetAsyncKeyState(VK_RCONTROL) & 0x8000)) return false;
+
+                auto inputs = std::array{
+                    // clang-format off
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_RCONTROL, .dwFlags = KEYEVENTF_KEYUP   | KEYEVENTF_EXTENDEDKEY}},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_DOWN,     .dwFlags = KEYEVENTF_KEYDOWN                        }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_DOWN,     .dwFlags = KEYEVENTF_KEYUP                          }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_RCONTROL, .dwFlags = KEYEVENTF_KEYDOWN | KEYEVENTF_EXTENDEDKEY}},
+                    // clang-format on
+                };
+                SendInput(static_cast<UINT>(inputs.size()), inputs.data(), sizeof(INPUT));
+
+                return true;
+            }
+
+            case 'P': { // RControl + P -> Up : PreviousLine
+                if (!(GetAsyncKeyState(VK_RCONTROL) & 0x8000)) return false;
+
+                auto inputs = std::array{
+                    // clang-format off
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_RCONTROL, .dwFlags = KEYEVENTF_KEYUP   | KEYEVENTF_EXTENDEDKEY}},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_UP,       .dwFlags = KEYEVENTF_KEYDOWN                        }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_UP,       .dwFlags = KEYEVENTF_KEYUP                          }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_RCONTROL, .dwFlags = KEYEVENTF_KEYDOWN | KEYEVENTF_EXTENDEDKEY}},
+                    // clang-format on
+                };
+                SendInput(static_cast<UINT>(inputs.size()), inputs.data(), sizeof(INPUT));
+
+                return true;
+            }
+
+            case 'T': { // RControl + T -> Shift + Right, Shift + Delete, Left, Shift + Insert : SwapCharacters
+                if (!(GetAsyncKeyState(VK_RCONTROL) & 0x8000)) return false;
+
+                auto inputs = std::array{
+                    // clang-format off
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_RCONTROL, .dwFlags = KEYEVENTF_KEYUP   | KEYEVENTF_EXTENDEDKEY}},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_SHIFT,    .dwFlags = KEYEVENTF_KEYDOWN                        }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_RIGHT,    .dwFlags = KEYEVENTF_KEYDOWN                        }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_RIGHT,    .dwFlags = KEYEVENTF_KEYUP                          }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_DELETE,   .dwFlags = KEYEVENTF_KEYDOWN                        }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_DELETE,   .dwFlags = KEYEVENTF_KEYUP                          }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_SHIFT,    .dwFlags = KEYEVENTF_KEYUP                          }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_LEFT,     .dwFlags = KEYEVENTF_KEYDOWN                        }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_LEFT,     .dwFlags = KEYEVENTF_KEYUP                          }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_SHIFT,    .dwFlags = KEYEVENTF_KEYDOWN                        }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_INSERT,   .dwFlags = KEYEVENTF_KEYDOWN                        }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_INSERT,   .dwFlags = KEYEVENTF_KEYUP                          }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_SHIFT,    .dwFlags = KEYEVENTF_KEYUP                          }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_RCONTROL, .dwFlags = KEYEVENTF_KEYDOWN | KEYEVENTF_EXTENDEDKEY}},
+                    // clang-format on
+                };
+                SendInput(static_cast<UINT>(inputs.size()), inputs.data(), sizeof(INPUT));
+
+                return true;
+            }
+
+            case 'Y': { // RControl + Y -> Shift + Insert : Yank
+                if (!(GetAsyncKeyState(VK_RCONTROL) & 0x8000)) return false;
+
+                auto inputs = std::array{
+                    // clang-format off
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_RCONTROL, .dwFlags = KEYEVENTF_KEYUP   | KEYEVENTF_EXTENDEDKEY}},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_SHIFT,    .dwFlags = KEYEVENTF_KEYDOWN                        }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_INSERT,   .dwFlags = KEYEVENTF_KEYDOWN                        }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_INSERT,   .dwFlags = KEYEVENTF_KEYUP                          }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_SHIFT,    .dwFlags = KEYEVENTF_KEYUP                          }},
+                    INPUT{.type = INPUT_KEYBOARD, .ki = {.wVk = VK_RCONTROL, .dwFlags = KEYEVENTF_KEYDOWN | KEYEVENTF_EXTENDEDKEY}},
+                    // clang-format on
+                };
+                SendInput(static_cast<UINT>(inputs.size()), inputs.data(), sizeof(INPUT));
+
+                return true;
+            }
 
             default:
                 return false;
             }
         }
+
         return false;
     }
 } // namespace ReControl::KeyBindings
