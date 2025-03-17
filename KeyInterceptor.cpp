f6c5ff65ec38d51bf01d023d3cfbf18e43124b69
Takuya Tsuchida <69303076+takuyatsuchida@users.noreply.github.com>
2025-03-18 01:30:39 +0900

    feat: add exclusion list for Key Interceptor to ignore specified foreground applications

--- KeyInterceptor.cpp
+++ KeyInterceptor.cpp
@@ -1,12 +1,46 @@
 ﻿#include "pch.h"
 #include "KeyInterceptor.h"
+#include <shlwapi.h>
 
 namespace
 {
     HHOOK hHook = nullptr;
 
+    const std::vector<std::wstring> EXCLUSION_LIST = {L"WindowsTerminal.exe", L"emacs.exe", L"gvim.exe"};
+
+    std::wstring GetForegroundApplicationExeName()
+    {
+        const auto hWnd = GetForegroundWindow();
+        if (!hWnd) return {};
+
+        DWORD processId = 0;
+        GetWindowThreadProcessId(hWnd, &processId);
+        if (!processId) return {};
+
+        const auto hProcess = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, FALSE, processId);
+        if (!hProcess) return {};
+
+        WCHAR exePath[MAX_PATH];
+        DWORD size = MAX_PATH;
+        const auto bResult = QueryFullProcessImageName(hProcess, 0, exePath, &size);
+        CloseHandle(hProcess);
+        if (!bResult) return {};
+
+        return PathFindFileName(exePath);
+    }
+
+    bool IsForegroundApplicationExcluded(const std::wstring &exeName)
+    {
+        return std::ranges::find(EXCLUSION_LIST, exeName) != EXCLUSION_LIST.end();
+    }
+
     LRESULT CALLBACK LowLevelKeyboardProc(const int nCode, const WPARAM wParam, const LPARAM lParam)
     {
+        if (nCode != HC_ACTION) return CallNextHookEx(nullptr, nCode, wParam, lParam);
+
+        const auto exeName = GetForegroundApplicationExeName();
+        if (IsForegroundApplicationExcluded(exeName)) return CallNextHookEx(nullptr, nCode, wParam, lParam);
+
         return CallNextHookEx(nullptr, nCode, wParam, lParam);
     }
 } // namespace
